7355d13e29cda6eba3696cd09a751ffa567f71ae
Red Hawk Eye <90817826+redhawkeye@users.noreply.github.com>
2024-04-16 12:22:50 +0000

    Update README

--- startup.sh
+++ startup.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-mkdir -p ~/.vnc/
+if [ ! -d ~/.vnc/ ]; then
+    mkdir -p ~/.vnc/
+fi
+if [ ! -f ~/.Xauthority ]; then
+    touch ~/.Xauthority
+fi
 rm -rf ~/.vnc/*.pid ~/.vnc/*.log /tmp/.X1*
-touch ~/.Xauthority
-vncpasswd -f <<< $PASSWORD > ~/.vnc/passwd
+vncpasswd -f <<< ${PASSWORD} > ~/.vnc/passwd
 vncserver -PasswordFile ~/.vnc/passwd
 sudo dbus-daemon --config-file=/usr/share/dbus-1/system.conf
 /usr/share/novnc/utils/novnc_proxy --listen 8080 --vnc 127.0.0.1:5901
